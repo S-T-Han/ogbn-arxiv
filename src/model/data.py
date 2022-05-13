408d8538664b437ce57bd40a6faa43f4d6de8cc7
S-T-Han <1962694160@qq.com>
2022-05-13 23:55:55 +0800

    add self loop to the graph

--- src/model/data.py
+++ src/model/data.py
@@ -1,5 +1,6 @@
 import os
 from ogb.nodeproppred import DglNodePropPredDataset
+import torch
 import dgl
 
 
@@ -13,14 +14,21 @@ class Arxiv:
         dataset = DglNodePropPredDataset(name='ogbn-arxiv', root=self.data_path)
         self.g, self.labels = dataset[0]
         self.g.add_edges(*self.g.all_edges()[::-1])
-        self.g.remove_self_loop().add_self_loop()
+        self.g = self.g.remove_self_loop().add_self_loop()
         self.idx = dataset.get_idx_split()
 
 
 if __name__ == "__main__":
     arxiv = Arxiv()
-    print(type(arxiv.labels))
-    print(arxiv.labels[: 20])
     print(arxiv.g.ndata['feat'].shape[-1])
+    graph = arxiv.g
+    assert isinstance(graph, dgl.DGLGraph)
+    src, dst = graph.all_edges()
+    print(src[: 5])
+    print(dst[: 5])
+    print(graph.has_edges_between(torch.tensor([104447]), torch.tensor([13091])))
+    print(graph.has_edges_between(torch.tensor([13091]), torch.tensor([104447])))
+    print(graph.has_edges_between(torch.tensor([104447]), torch.tensor([104447])))
+
 
 
